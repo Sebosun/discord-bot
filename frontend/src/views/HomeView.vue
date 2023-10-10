b28ad30a539b590cfa60b778e78f4e30708cfacc
Seb <seb@192.168.1.5>
2023-10-10 03:06:38 +0200

    feat(frontend): file upload get file

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,24 @@
 <script setup lang="ts">
-console.log('test')
+const baseURL = 'localhost:3000'
+const onFileChanged = async (event: Event) => {
+  const target = event.target
+  console.log(target.files[0])
+
+  /* await fetch(`${baseURL}/upload-file`, { */
+  /*   method: 'POST', */
+  /*   headers: { */
+  /*     'Content-Type': 'application/json' */
+  /*   }, */
+  /*   body: file */
+  /* }) */
+}
 </script>
 
 <template>
   <main>
-    <h1 class="text-center text-8xl">Test</h1>
+    <h1 class="text-center text-8xl mb-20">Image upload app</h1>
+    <div class="text-center">
+      <input type="file" @change="onFileChanged" accept="image/*" capture />
+    </div>
   </main>
 </template>
