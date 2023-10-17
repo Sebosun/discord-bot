4bbd04aec5871b54116d4f4b6c65c6ba593b226b
Sebosun <sebos@protonmail.com>
2023-10-17 20:22:29 +0200

    feat(server): twt link parser

--- server/src/helpers/parseTwitterLink.ts
+++ server/src/helpers/parseTwitterLink.ts
@@ -1,10 +1,16 @@
 const BASE_REGEX =
     /https?:\/\/(?:(?:www|m(?:obile)?)\.)?(?:twitter\.com|twitter3e4tixl4xyajtrzo62zg5vztmjuricljdp2c5kshju4avyoid\.onion|x\.com)/;
 
+/* const queryReg = /(\?.+)/ */
+const queryReg = /\?.+/g;
+const removeQueryFromUrl = (link: string) => link.replace(queryReg, '');
+
 export function parseTwitterLink(link: string) {
     const twtSiteWithPreviews = 'https://vxtwitter.com';
 
-    const newStr = link.replace(BASE_REGEX, twtSiteWithPreviews);
+    console.log('new str');
+    let newStr = link.replace(BASE_REGEX, twtSiteWithPreviews);
+    newStr = removeQueryFromUrl(newStr);
     if (!newStr) {
         throw new Error('Not a valid twitter link');
     }
--- server/src/tests/parseTwitterLink.test.ts
+++ server/src/tests/parseTwitterLink.test.ts
@@ -1,13 +1,19 @@
 // sum.test.js
-import { expect, test } from 'vitest';
+import { describe, expect, test } from 'vitest';
 import { parseTwitterLink } from '../helpers/parseTwitterLink';
 
-test('Normal twitter link', () => {
-    const str = 'https://twitter.com/i/cards/tfw/v1/623160978427936768';
-    expect(parseTwitterLink(str)).toEqual('https://vxtwitter.com/i/cards/tfw/v1/623160978427936768');
-});
+describe('twt links', () => {
+    test('Normal twitter link', () => {
+        const str = 'https://twitter.com/i/cards/tfw/v1/623160978427936768';
+        expect(parseTwitterLink(str)).toEqual('https://vxtwitter.com/i/cards/tfw/v1/623160978427936768');
+    });
 
-test('X.com', () => {
-    const str = 'https://x.com/i/cards/tfw/v1/623160978427936768';
-    expect(parseTwitterLink(str)).toEqual('https://vxtwitter.com/i/cards/tfw/v1/623160978427936768');
+    test('X.com', () => {
+        const str = 'https://x.com/i/cards/tfw/v1/623160978427936768';
+        expect(parseTwitterLink(str)).toEqual('https://vxtwitter.com/i/cards/tfw/v1/623160978427936768');
+    });
+    test('Removes queries base', () => {
+        const str = 'https://twitter.com/i/cards/tfw/v1/623160978427936768?sranie-dupa';
+        expect(parseTwitterLink(str)).toEqual('https://vxtwitter.com/i/cards/tfw/v1/623160978427936768');
+    });
 });
